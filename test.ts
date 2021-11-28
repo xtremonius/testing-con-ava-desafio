acf3317baa628af107a6adfde1171560d1fb7a21
xtremonius <alejandro.aquino1988@gmail.com>
2021-11-28 19:34:05 -0300

    Proyecto modificado

--- test.ts
+++ test.ts
@@ -1,4 +1,19 @@
 import test from "ava";
+import { User } from "./index";
+import { Product } from "./index";
+/**
+ * o import {User, Product} from "./index";
+ */
 
-test("Testeo la clase User", (t) => {});
-test("Testeo la clase Product y sus métodos", (t) => {});
+test("Testeo la clase User", (t) => {
+  const newUser = new User("Alejandro");
+  t.is(newUser.name, "Alejandro");
+});
+
+test("Testeo la clase Product y sus métodos", (t) => {
+  const newUser = new User("Alejandro");
+  const newProduct = new Product("Jabon", 100);
+  newUser.addProduct(newProduct);
+  t.is(newUser.products[0].name, "Jabon");
+  t.is(newUser.products[0].price, 100);
+});
